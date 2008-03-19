0d408cd4ba5f1d53fa437df28a32a1c65499ba46
long.mai.tamu <long.mai.tamu@096e12eb-2a47-0410-b48a-cfd01403d586>
2008-03-19 00:30:30 +0000

    updated servoscontroller and its dependencies

--- trunk/AutonomousSentryGun/Objects/Packet.cs
+++ trunk/AutonomousSentryGun/Objects/Packet.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using AutonomousSentryGun.Functions;
+
 namespace AutonomousSentryGun.Objects
 {
     class Packet
     {
-        private byte[] packet;
+        private byte[] data;
         private const int PACKET_BYTE_LENGTH = 5;
         private const byte HEADER_BYTE = 0xAA; //10101010
         //HEADER:7
@@ -20,36 +22,44 @@ namespace AutonomousSentryGun.Objects
         private const int YPOSITION_IDX = 0;
 
         public Packet() {
-            packet = new byte[PACKET_BYTE_LENGTH];
+            data = new byte[PACKET_BYTE_LENGTH];
         }
-        //public string toString() { }
-        public byte[] getPacket()
+        
+        public byte[] Data
         {
-            return packet;
+          get
+          {
+            return data;
+          }
         }
         public void validatePacket()
         {
             invalidatePacket();
-            packet[HEADER_IDX] |= HEADER_BYTE;
+            data[HEADER_IDX] |= HEADER_BYTE;
         }
         public void invalidatePacket()
         {
-            packet[HEADER_IDX] &= 1;
+            data[HEADER_IDX] &= 1;
         }
         public void setFireOn()
         {
-            packet[FIRE_IDX] |= 1;
+            data[FIRE_IDX] |= 1;
         }
         public void setFireOff()
         {
-            packet[FIRE_IDX] &= Convert.ToByte("11111110", 2);
+            data[FIRE_IDX] &= Convert.ToByte("11111110", 2);
+        }
+        public void setPosition(Position position)
+        {
+          this.setXPosition(position.X);
+          this.setYPosition(position.Y);
         }
         public void setXPosition(int position)
         {
             byte[] positionBytes = IntToTwoBytes(position);
            
             for(int i=0; i<positionBytes.Length; i++) 
-                packet[XPOSITION_IDX+i] = positionBytes[i];
+                data[XPOSITION_IDX+i] = positionBytes[i];
             
         }
         public void setYPosition(int position)
@@ -57,7 +67,7 @@ namespace AutonomousSentryGun.Objects
             byte[] positionBytes = IntToTwoBytes(position);
 
             for (int i = 0; i < positionBytes.Length; i++)
-                packet[YPOSITION_IDX + i] = positionBytes[i];
+                data[YPOSITION_IDX + i] = positionBytes[i];
         }
         private byte[] IntToTwoBytes(int n)
         {
